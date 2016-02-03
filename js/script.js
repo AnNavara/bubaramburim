f42ec1ef2fe6841d5d92bb1719d8233866b6fb65
AnNavara <Navarachkou@gmail.com>
2016-02-03 06:01:57 +0500

    something something

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
   var nav_sandwich  = document.querySelector('.page-nav__control');
   var nav_dropdown  = document.querySelector('.page-nav__drop');
 
-  nav_sandwich.addEventListener("click", function(e) {
+  nav_sandwich.addEventListener('click', function(e) {
     e.preventDefault;
-    nav_dropdown.classList.toggle("page-nav__drop--active");
+    nav_dropdown.classList.toggle('page-nav__drop--active');
   });
 })();
